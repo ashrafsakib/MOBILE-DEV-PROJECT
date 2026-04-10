2ae22bccb10038d3aef30b8fc6113ab0c20fcd47
ashrafsakib <ashrafulsakib740@gmail.com>
2026-04-10 14:50:35 +0545

    added document bloc and repository

--- lib/core/firestore/schemas/schemas.dart
+++ lib/core/firestore/schemas/schemas.dart
@@ -1,4 +1,5 @@
 export 'app_user_schema.dart';
 export 'firestore_collections_schema.dart';
+export 'university_application_schema.dart';
 export 'university_program_schema.dart';
 export 'university_schema.dart';
