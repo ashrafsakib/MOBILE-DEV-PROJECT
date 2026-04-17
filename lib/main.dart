765e748297ec2b4b1b3361b40c4421658c57aaca
Tanvir3135 <t3ahta00@students.oamk.fi>
2026-04-17 17:44:01 +0545

    added reminder feature

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await setupDependencies();
+
   runApp(const MyApp());
 }
 
